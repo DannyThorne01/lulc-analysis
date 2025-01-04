a49de2b1e6bde8f2221f0d4d09a4474b909bf5ee
dsb289 <dsb289@cornell.edu>
2025-01-03 20:48:02 -0400

    Better Looking HeatMap

--- src/components/charts/heatmap.tsx
+++ src/components/charts/heatmap.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
+import data from "../../data/lc.json"
+
 
 interface Props {
   info: {
@@ -12,26 +14,78 @@ const HeatMap = ({ info }: Props) => {
   const svgRef = useRef<SVGSVGElement | null>(null);
   const w = 500;
   const h = 500;
-  const m = { top: 40, right: 30, bottom: 50, left: 60 };
+  const m = { top: 40, right: 30, bottom: 100, left: 100 };
   const heatmapWidth = w - m.left - m.right;
   const heatmapHeight = h - m.top - m.bottom;
+  const uniqueKeys = info.uniqueKeys.map((key) => {return(data.key_map[key.toString()])})
+  const transferMatrix = Object.entries(info['transferMatrix']).map(([key, value]) => {
+    const [before, after] = key.split("_").map((num) => parseInt(num, 10));
+    return { before, after, value };
+  });
+
+  
+
+  function findGroups() {
+    var groups = {};
+    var discovered = new Set();
+    uniqueKeys.forEach((key) => {
+      if (!discovered.has(key)) {
+        discovered.add(key);
+        let group = data.reverse_group_map[key];
+        if (group) {
+          if (!groups[group]) {
+            groups[group] = [key];
+          } else {
+            groups[group].push(key);
+          }
+        }
+      }
+    });
+    return groups;
+    
+  }
+
+  function makeReductions(){
+    var reductions: String[] =[]
+    var discovered = new Set();
+    uniqueKeys.forEach((key) => {
+      if (!discovered.has(key)) {
+        discovered.add(key);
+        let red:String = data.reductions[key]
+        if(red){
+          reductions.push(red)
+        }
+      }
+    })
+    return reductions
+  }
+  let reductions = makeReductions();
+  console.log(reductions)
+  
+  let xGroups=findGroups()
+  console.log(xGroups)
+  let new_keys = Object.values(xGroups).flat();
+  console.log(new_keys)
 
   useEffect(() => {
     d3.select(svgRef.current).selectAll("*").remove();
-
+    
     const d3svg = d3
-      .select(svgRef.current)
-      .attr("width", "100%")
-      .attr("height", "100%")
-      .attr("viewBox", `0 0 ${w} ${h}`)
-      .attr("preserveAspectRatio", "xMinYMin meet")
-      .style("background-color", "#ffffff");
+    .select(svgRef.current)
+    .attr("viewBox", `0 0 ${w} ${h}`)
+    .style("background-color", "#d9d9d9");
 
     // Annotation groups for axes
+    const yAnnotations = d3svg
+    .append("g")
+    .attr("class", "y-group-annotations")
+    .attr("transform", `translate(${m.left - 10}, ${m.top})`);
+
     const heatmapXAxis = d3svg
       .append("g")
       .attr("class", "axis")
       .attr("transform", `translate(${m.left}, ${heatmapHeight + m.top + 10})`);
+
     const heatmapYAxis = d3svg
       .append("g")
       .attr("class", "axis")
@@ -39,21 +93,28 @@ const HeatMap = ({ info }: Props) => {
 
     // Scales and colors
     const xScale = d3
-      .scaleBand<number>()
+      .scaleBand()
       .range([0, heatmapWidth])
-      .domain(info.uniqueKeys)
-      .padding(0.05);
+      .domain(reductions)
+      .padding(.2);
 
     const yScale = d3
-      .scaleBand<number>()
+      .scaleBand()
       .range([heatmapHeight, 0])
-      .domain(info.uniqueKeys)
-      .padding(0.05);
+      .domain(reductions)
+      .padding(.2);
 
     const myColor = d3
-      .scaleLinear<number>()
+      .scaleLinear()
       .range(["white", "#69b3a2"])
-      .domain([0, 1]);
+      .domain([0, 10])
+      .clamp(true)
+    
+    const domain = Object.keys(data.group_map);
+    const colorScale = d3
+    .scaleOrdinal()
+    .domain(Object.keys(xGroups)) // Use the keys of xGroups as the domain
+    .range(["red", "blue", "green", "orange", "purple", "pink", "cyan", "yellow"]);
 
     // Axes
     const xAxis = d3.axisBottom(xScale);
@@ -61,12 +122,110 @@ const HeatMap = ({ info }: Props) => {
     heatmapXAxis.call(xAxis);
     heatmapYAxis.call(yAxis);
 
-    // Convert `matrix` data into a usable format
-    const transferMatrix = Object.entries(info['transferMatrix']).map(([key, value]) => {
-      const [before, after] = key.split("_").map((num) => parseInt(num, 10));
-      return { before, after, value };
+    Object.entries(xGroups).forEach((entry: [string, string[]]) => {
+      console.log(entry[0]);
+    
+      const yStart = yScale(data.reductions[entry[1][0]]);
+      const yEnd = yScale(data.reductions[entry[1][entry[1].length - 1]]);
+      console.log(yStart, yEnd);
+    
+      // Add line annotation
+      yAnnotations
+        .append("line")
+        .attr("x1", 0)
+        .attr("x2", 0)
+        .attr("class", entry[0])
+        .attr("y1", yStart+10)
+        .attr("opacity", "0.4")
+        .attr("y2", yEnd)
+        .attr("stroke", colorScale(entry[0]))
+        .attr("stroke-width", 5);
+    });
+    Object.entries(xGroups).forEach((entry: [string, string[]]) => {
+      console.log(entry[0]);
+    
+      const xStart = xScale(data.reductions[entry[1][0]]);
+      const xEnd = xScale(data.reductions[entry[1][entry[1].length - 1]]);
+    
+      // Add line annotation
+      yAnnotations
+        .append("line")
+        .attr("x1", xStart+10)
+        .attr("x2", xEnd)
+        .attr("class", entry[0])
+        .attr("y1", heatmapHeight +10)
+        .attr("opacity", "0.4")
+        .attr("y2", heatmapHeight +10)
+        .attr("stroke", colorScale(entry[0]))
+        .attr("stroke-width", 5);
+    });
+    
+    // Adding Text Labels
+    Object.entries(xGroups).forEach((entry: [string, string[]]) => {
+      const group = entry[0];
+      const yStart = yScale(data.reductions[entry[1][0]]);
+      const yEnd = yScale(data.reductions[entry[1][entry[1].length - 1]]);
+      if (Math.abs(yStart - yEnd) < 10) {
+        return; // Skip this iteration and move to the next
+      }
+      // // Add text annotation
+      // yAnnotations
+      // .append("text")
+      // .attr("transform", `rotate(-90, 25, ${(yStart + yEnd) / 2})`) // Rotate around the midpoint
+      // .attr("x", 25) // Offset text horizontally
+      // .attr("y", (yStart + yEnd) / 2) // Position at the midpoint
+      // .style("font-size", "10px") // Customize font size
+      // .style("fill", "black") // Customize text color
+      // .text(group); // Add the group name as text
     });
 
+
+    // Customize x-axis labels
+    heatmapXAxis.selectAll("text")
+  .attr("transform", function() {
+    // Rotate each text element around its current position
+    return `rotate(-90, ${d3.select(this).attr("x") || 0}, ${d3.select(this).attr("y") || 0})`;
+  })
+  .attr("x", 0) // Adjust to position closer to the tick
+  .attr("y", 5)  // Adjust to position away from the axis
+  .style("font-size", "10px")
+  .style("fill", "#4A4A4A")
+  .style("font-family", "Arial, sans-serif")
+  .style("font-weight", "bold")
+  .style("text-anchor", "end");
+
+    // Customize y-axis labels
+    heatmapYAxis.selectAll("text")
+    .style("font-size", "10px")
+    .style("fill", "#4A4A4A") 
+    .style("font-family", "Arial, sans-serif") 
+    .style("font-weight", "bold"); 
+
+    // Break labels into multiple lines and adjust spacing
+    heatmapYAxis.selectAll("text")
+    .each(function () {
+      const text = d3.select(this);
+      const words = text.text().split(' '); // Split text into words
+      const lineHeight = 1.1; // Line height (em units)
+      const y = text.attr("y");
+      const dy = parseFloat(text.attr("dy")) || 0;
+
+      // Clear original text
+      text.text(null);
+
+      // Add each word as a separate line
+      words.forEach((word, i) => {
+        text.append("tspan")
+          .text(word)
+          .attr("x", -m.left/10) 
+          .attr("y", y) 
+          // .attr("dy", `${dy + i * lineHeight}em`) // Position each line
+          .style("text-anchor", "end")
+      
+      });
+    });
+    
+    console.log(transferMatrix)
     // Draw heatmap rectangles
     d3svg
       .selectAll(".tile")
@@ -74,8 +233,10 @@ const HeatMap = ({ info }: Props) => {
       .enter()
       .append("rect")
       .attr("class", "tile")
-      .attr("x", (d) => xScale(d.after)! + m.left)
-      .attr("y", (d) => yScale(d.before)! + m.top)
+      .attr("x", (d) => xScale(data.reductions[data.key_map[d.after.toString()]])! + m.left)
+      .attr("y", (d) => yScale(data.reductions[data.key_map[d.before.toString()]])! + m.top)
+      .attr("rx", 2) // Set the x-axis corner radius
+      .attr("ry", 2) // Set the y-axis corner radius
       .attr("width", xScale.bandwidth())
       .attr("height", yScale.bandwidth())
       .style("fill", (d) => myColor(d.value));
